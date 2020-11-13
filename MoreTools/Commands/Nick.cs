2d76ba800d5271049ee920ba045f5b34cf64f5fe
GrafDimenzio <crafterwolfhd@gmail.com>
2020-11-13 17:19:49 +0100

    added .nick Command

---
+++ MoreTools/Commands/Nick.cs
@@ -0,0 +1,39 @@
+﻿using Synapse.Command;
+
+namespace MoreTools.Commands
+{
+    [CommandInformation(
+        Name = "Nick",
+        Aliases = new string[] { "NickName" },
+        Description = "A Command to change your Nickname",
+        Permission = "moretools.nick",
+        Platforms = new Platform[] { Platform.ClientConsole },
+        Usage = ".nick nickname"
+        )]
+    public class Nick : ISynapseCommand
+    {
+        public CommandResult Execute(CommandContext context)
+        {
+            var result = new CommandResult();
+
+            if (!context.Player.HasPermission("moretools.nick"))
+            {
+                result.Message = "You don't have permission to change your nickname!";
+                result.State = CommandResultState.NoPermission;
+                return result;
+            }
+
+            if(context.Arguments.Count < 1)
+            {
+                result.Message = "Missing Parameter! Usage: .nick nickname";
+                result.State = CommandResultState.Error;
+            }
+
+
+            context.Player.DisplayName = context.Arguments.FirstElement();
+            result.Message = $"Your Nickname is now {context.Arguments.FirstElement()}";
+            result.State = CommandResultState.Ok;
+            return result;
+        }
+    }
+}
--- MoreTools/PluginClass.cs
+++ MoreTools/PluginClass.cs
@@ -5,11 +5,11 @@ namespace MoreTools
     [PluginInformation(
         Name = "MoreTools",
         Author = "Dimenzio",
-        Description = "A Plugin which adds a ton off Tools for Admins to the game",
+        Description = "A Plugin which adds a ton of Tools for Admins to the game",
         LoadPriority = int.MinValue,
-        SynapseMajor = SynapseController.SynapseMajor,
-        SynapseMinor = SynapseController.SynapseMinor,
-        SynapsePatch = SynapseController.SynapsePatch,
+        SynapseMajor = 2,
+        SynapseMinor = 0,
+        SynapsePatch = 1,
         Version = "v.1.0.0"
         )]
     public class PluginClass : AbstractPlugin { }
